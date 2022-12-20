2590bc51bdde6ab6030fb78fec47236003a518db
Richard Muri <muri.engineering@gmail.com>
2022-12-19 22:56:24 -0500

    Solved day4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,9 +2,36 @@ use std::fs;
 use std::io;
 use std::io::BufRead;
 
+use parse_display::{Display, FromStr};
+
+#[derive(Copy, Clone, Display, FromStr, Debug)]
+#[display("{min}-{max}")]
+struct Section {
+    min: u64,
+    max: u64,
+}
+
+impl Section {
+    fn contains(&self, other: &Section) -> bool {
+        self.min <= other.min && self.max >= other.max
+    }
+
+    fn overlaps(&self, other: &Section) -> bool {
+        (self.min <= other.min && self.max >= other.min) ||
+        (self.min <= other.max && self.max >= other.min)
+    }
+}
+
+#[derive(Display, FromStr, Debug)]
+#[display("{a},{b}")]
+struct Pair {
+    a: Section,
+    b: Section,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = common::Args::init();
-    let filepath = common::get_input_path("dayX", &args.file).unwrap();
+    let filepath = common::get_input_path("day4", &args.file).unwrap();
     let file = match fs::File::open(&filepath) {
         Err(err) => panic!("Failed to open {}: {}", filepath.display(), err),
         Ok(file) => file,
@@ -14,9 +41,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let lines = io::BufReader::new(file).lines();
 
-    for line in lines{
+    let pairings = lines
+        .map(|line| line.unwrap().parse::<Pair>())
+        .collect::<Result<Vec<Pair>, _>>()?;
 
-    }
+    // println!("{:?}", pairings);
+
+    let contains = pairings
+        .iter()
+        .filter(|pairing| pairing.a.contains(&pairing.b) || pairing.b.contains(&pairing.a))
+        .count();
+
+    println!("{contains} pairings fully contain each other.");
+
+    let overlaps = pairings
+    .iter()
+    .filter(|pairing| pairing.a.overlaps(&pairing.b))
+    .count();
+
+    println!("{overlaps} pairings overlap with each other.");
+
+    // for line in lines{
+    // }
 
     Ok(())
 }
